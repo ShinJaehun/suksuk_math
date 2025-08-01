0658837d0e086aa5cd8f3e3d68c7697712c8be61
ShinJaehun <gunsmoke.shin@gmail.com>
2025-08-01 11:40:29 +0900

    legacy code 삭제

--- app/src/main/java/com/shinjaehun/suksuk/domain/model/DivisionProblem.kt
+++
@@ -1,9 +0,0 @@
-package com.shinjaehun.suksuk.domain.model
-
-// legacy code
-
-//data class DivisionProblem(
-//    val dividend: Int,
-//    val divisor: Int,
-//    val quotient: Int
-//)
--- app/src/main/java/com/shinjaehun/suksuk/presentation/division21/Division21UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.shinjaehun.suksuk.presentation.division21
-
-// legacy code
-
-//data class Division21UiState(
-//    val problem: DivisionProblem,
-//    val userInput: String = "",
-//    val result: Boolean? = null
-//)
